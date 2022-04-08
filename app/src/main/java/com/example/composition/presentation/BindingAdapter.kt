314117f342fac213821b5628d8377596db2650fd
NikaNed <babenkova.nika@ya.ru>
2022-04-08 22:35:36 +0300

    Data Binding Adapter for GameFinishFragment

---
+++ app/src/main/java/com/example/composition/presentation/BindingAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.composition.presentation
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.composition.R
+import com.example.composition.domain.entites.GameResult
+
+@BindingAdapter("requiredAnswers")
+fun bindRequiredAnswers(textView: TextView, count: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.required_answer),
+        count
+    )
+}
+
+@BindingAdapter("requiredPercentage")
+fun bindRequiredPercentage(textView: TextView, percentage: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.required_percentage),
+        percentage
+    )
+}
+
+@BindingAdapter("scoreAnswer")
+fun bindScoreAnswer(textView: TextView, score: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.score_answer),
+        score
+    )
+}
+
+@BindingAdapter("scorePercentage")
+fun bindScorePercentage(textView: TextView, gameResult: GameResult) {
+    textView.text = String.format(
+        textView.context.getString(R.string.score_percentage),
+        getPercentOfRightAnswers(gameResult)
+    )
+}
+
+private fun getPercentOfRightAnswers(gameResult: GameResult) = with(gameResult) {
+    if (countOfQuestions == 0) {
+        return 0
+    }
+    ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
+}
+
+@BindingAdapter("emojiResult")
+fun bindEmojiResult(imageView: ImageView, winner: Boolean) {
+    imageView.setImageResource(getSmileResId(winner))
+}
+
+private fun getSmileResId(winner: Boolean): Int {
+    return if (winner) {
+        R.drawable.ic_emoji_happy
+    } else {
+        R.drawable.ic_emoji_sad
+    }
+}
--- app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
@@ -7,9 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.example.composition.R
 import com.example.composition.databinding.GameFinishedFragmentBinding
-import com.example.composition.domain.entites.GameResult
 
 class GameFinishedFragment : Fragment() {
 
@@ -30,48 +28,13 @@ class GameFinishedFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupOnClickListener()
-        bindViews()
-    }
-
-    private fun bindViews() {
-        with(binding) {
-            emojiResult.setImageResource(getSmileResId())
-            tvRequiredAnswer.text = String.format(
-                getString(R.string.required_answer),
-                args.gameResul.gameSettings.minCountOfRightAnswers
-            )
-            tvScoreAnswer.text = String.format(
-                getString(R.string.score_answer),
-                args.gameResul.countOfRightAnswers
-            )
-            tvRequiredPercentage.text = String.format(
-                getString(R.string.required_percentage),
-                args.gameResul.gameSettings.minPercentOfRightAnswers
-            )
-            tvScorePercentage.text = String.format(
-                getString(R.string.score_percentage),
-                getPercentOfRightAnswers()
-            )
-        }
-    }
-
-    private fun getSmileResId(): Int {
-        return if (args.gameResul.winner)
-            R.drawable.ic_emoji_happy
-        else R.drawable.ic_emoji_sad
+        binding.gameResult = args.gameResult
     }
 
     private fun setupOnClickListener() {
         binding.buttonRetry.setOnClickListener {
             retryGame()
         }
-    }
-
-    private fun getPercentOfRightAnswers() = with(args.gameResul) {
-        if (countOfQuestions == 0) {
-            return 0
-        }
-        ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
     }
 
     private fun retryGame() {
